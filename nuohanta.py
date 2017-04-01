0d1d90019229e2a147abca8eafc72ef184ae947e
chentf <chentf@mchz.com.cn>
2017-04-01 09:39:26 +0800

    汉诺塔  获取网卡 get-pip

---
+++ nuohanta.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+#汉诺塔方法
+B=[]
+def move(n,a,b,c):
+	if n == 1:
+		buzhou = a + str(n) + '-->' + c + str(n)
+		B.append(buzhou)
+		return
+	else:
+		move(n-1,a,c,b)#n-1个盘子从a-b
+		move(1,a,b,c)#1个盘子从a-c
+		move(n-1,b,a,c)#n-1个盘子从b-c
+move(3,'A','B','C')
+print(B)
